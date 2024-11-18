889023ab215706c5f1d2dae1fdf7877bef623e36
kaami419 <tareen544@gmail.com>
2024-11-18 20:59:59 +0500

    auth added

--- tooltip-be/routes/imageRoutes.js
+++ tooltip-be/routes/imageRoutes.js
@@ -9,7 +9,7 @@ const path = require("path");
 // Configure storage options for multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "uploads/")); // Set the destination folder for uploaded files
+    cb(null, path.join(__dirname, "../uploads/")); // Set the destination folder for uploaded files
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
